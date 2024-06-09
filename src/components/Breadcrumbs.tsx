6ea3b7003e7fae806b17354f072a5c35f423ca96
Jakub Šrámek <sramekvm@gmail.com>
2024-06-09 16:59:40 +0200

    Breadcrumbs component

---
+++ src/components/Breadcrumbs.tsx
@@ -0,0 +1,38 @@
+import { UIMatch, useLocation, useMatches } from "react-router-dom";
+
+interface HandleType {
+  crumb: (title?: string) => JSX.Element;
+}
+
+const Breadcrumbs = () => {
+  const matches = useMatches() as UIMatch<null, HandleType>[];
+  const crumbs = matches.map((mtch) => mtch.handle?.crumb).filter(Boolean);
+  const location = useLocation();
+
+  return (
+    <ol
+      style={{
+        display: "flex",
+        alignItems: "center",
+        whiteSpace: "pre",
+        listStyleType: "none",
+      }}
+    >
+      {crumbs.map((crumb, index) => {
+        const isLast = index + 1 === crumbs.length;
+        return (
+          <li key={index}>
+            <span style={{ textDecoration: "underline" }}>
+              {crumb(location.state?.userName)}
+            </span>
+            {!isLast && (
+              <span style={{ pointerEvents: "none", padding: "5px" }}>/</span>
+            )}
+          </li>
+        );
+      })}
+    </ol>
+  );
+};
+
+export default Breadcrumbs;
